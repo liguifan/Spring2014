174cbb45393d688e1fc7ff50205e2db10468f7a0
Guifan Li <liguifan@Guifans-MacBook-Pro.local>
2014-01-06 02:31:37 -0500

    evaluate_reverse_polish_notation

--- Leetcode/arraylist/permutation.java
+++ Leetcode/arraylist/permutation.java
@@ -42,14 +42,22 @@ public static void main(String[] args){
             ArrayList<Integer> list = new ArrayList<Integer>();
             list.add(num[index]);
             result.add(list);
+            System.out.println("3333333333333 "+list);
             return result;
         }else{
             ArrayList<ArrayList<Integer>> partial = permuteHelper(num, index + 1);
+            System.out.println(partial);
             for(ArrayList<Integer> list: partial){
+            	System.out.println("ober "+list);
                 for(int i = 0; i <= list.size(); i++){
+                	
                     ArrayList<Integer> tmp = new ArrayList<Integer>(list);
                     tmp.add(i, num[index]);
+                    // add an element to every list inside the results. so eveything 
+                    // become from size 2 to size 3. like [2,3] [3,2] -> [1,2,3] [1,3,2]
+                    System.out.println("before 1"+tmp);
                     result.add(tmp);
+                    System.out.println("after 2"+result);
                 }
             }
             return result;
