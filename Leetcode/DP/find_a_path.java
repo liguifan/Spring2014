edbe1a1333dbf274640a9155d0989a3a0ac715d2
Guifan Li <liguifan@Guifans-MacBook-Pro.local>
2014-01-11 22:45:08 -0500

    zigzag

--- Leetcode/DP/find_a_path.java
+++ Leetcode/DP/find_a_path.java
@@ -9,28 +9,38 @@ public class find_a_path {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Point> path1=new ArrayList<Point>();
+		Hashtable<Point,Boolean> cache=new Hashtable<Point,Boolean>();
+		
+		getPath(4,5,path1,cache);
+		
+		for(Point p:path1){
+			System.out.println(p.x+" y "+p.y);
+		}
+		return;
 	}
 
 	
-	public boolean getPath(int x, int y, ArrayList<Point> path, Hashtable<Point,Boolean> cache){
+	public static boolean getPath(int x, int y, ArrayList<Point> path, Hashtable<Point,Boolean> cache){
 		Point p=new Point(x,y);
 		if(cache.containsKey(p)){
 			return cache.get(p);
 		}
 		
 		if(x==0 && y==0){
+			System.out.println("why");
 			return true;
 		}
 		
 		boolean success =false;
-		if(x>=1 && x-1!=0){
+		if(x>=1 && x!=0){
 			success=getPath(x-1,y,path,cache);
 		}
-		if(!success && y>=1 && y-1!=0){
+		if(!success && y>=1 && y!=0){
 			success=getPath(x,y-1,path,cache);
 		}
 		if(success){
+			System.out.println("test");
 			path.add(p);
 		}
 		cache.put(p, success);
