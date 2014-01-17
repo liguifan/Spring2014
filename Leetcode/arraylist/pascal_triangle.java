4bc0f4b093510bd754663f668d216042f8aa5cff
Guifan Li <liguifan@dyn-209-2-211-196.dyn.columbia.edu>
2014-01-16 19:25:57 -0500

    pascal triangle DP

--- Leetcode/arraylist/pascal_triangle.java
+++ Leetcode/arraylist/pascal_triangle.java
@@ -9,43 +9,31 @@ public class pascal_triangle {
 
 		
 		ArrayList<Integer> a=new ArrayList<Integer>();
-		ArrayList<ArrayList<Integer>> result=generate(5);
-		
-		for(ArrayList<Integer> x:result ){
+		ArrayList<ArrayList<Integer>> result=generate(3);
+
+		for(ArrayList<Integer> x:result){
 			System.out.println(x);
 		}
-		
 	}
 	
 	public static ArrayList<ArrayList<Integer>> generate(int n) {
 		ArrayList<ArrayList<Integer>> set=new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> last= new ArrayList<Integer>();
 		ArrayList<Integer> first= new ArrayList<Integer>();
-		ArrayList<Integer> second= new ArrayList<Integer>();
+		ArrayList<Integer> last= new ArrayList<Integer>();
 		first.add(1);
-		second.add(1);
-		second.add(1);
-		
 		last.add(1);
-		last.add(2);
 		last.add(1);
+	
 		if(n==0)  return set;
 		else if(n==1){
 			
 			set.add(first);
 			return set;
-		}
-		else if(n==2)
-		{
-			
-			set.add(first);
-			set.add(second);
-			return set;
+		
 		}else{
 			set.add(first);
-			set.add(second);
 			set.add(last);
-			int j=3;
+			int j=2;
 			while(j<=n-1){
 				ArrayList<Integer> path= new ArrayList<Integer>();
 				path.add(1);
@@ -59,6 +47,32 @@ public class pascal_triangle {
 			}
 		}		
 		return set;
+    }
+	
+	
+	public static ArrayList<Integer> getRow(int n) {
+		ArrayList<Integer> path= new ArrayList<Integer>();
+		ArrayList<Integer> first= new ArrayList<Integer>();
+		ArrayList<Integer> last= new ArrayList<Integer>();
+		first.add(1);
+		last.add(1);
+		last.add(1);
+		
+		if(n==0) return first;
+		if(n==1) return last;
+		int j=1;
+		while(j<=n-1){
+			path.clear();
+			
+			path.add(1);
+			for(int i=0;i<=last.size()-2;i++){
+				path.add(last.get(i)+last.get(i+1));
+			}
+			path.add(1);
+			last=new ArrayList<Integer>(path);
+			j++;
+		}
+		return path;
     }
 
 }
