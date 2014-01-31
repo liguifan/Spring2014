608c821edb89ea2ba9b6fe63b012b4d4c6effe76
liguifan <liguifan@gmail.com>
2014-01-30 22:01:05 -0500

    kth smallest and kth largest

--- Leetcode/heap_manipulation/heap_implementation.java
+++ Leetcode/heap_manipulation/heap_implementation.java
@@ -24,50 +24,7 @@ public class heap_implementation {
 	
 	
 	
-	public static int[] find_kth_smallest(int[] arr,int k){
-		PriorityQueue<Integer> queue=new PriorityQueue<Integer>(k,Collections.reverseOrder());
-		for(int i=0;i<=k-1;i++){
-			queue.add(arr[i]);
-		}
-		for(int i=k;i<=arr.length-1;i++){
-			if(arr[i]>queue.peek()){
-				System.out.println("peek "+queue.peek());
-				i++;
-			}else
-			{
-				queue.poll();
-				queue.add(arr[i]);
-			}
-		}
-		int[] b=new int[k];
-		for(int i=k-1;i>=0;i--){
-			b[i]=queue.poll();
-		}
-		return b;
-	}
 	
-	public static int[] find_kth_largest(int[] arr,int k){
-		PriorityQueue<Integer> queue=new PriorityQueue<Integer>();
-		for(int i=0;i<=k-1;i++){
-			queue.add(arr[i]);
-		}
-		for(int i=k;i<=arr.length-1;i++){
-			if(arr[i]<queue.peek()){
-				i++;
-			}else
-			{
-				queue.poll();
-				queue.add(arr[i]);
-			}
-		}
-		int[] b=new int[k];
-		for(int i=k-1;i>=0;i--){
-			b[i]=queue.poll();
-		}
-		return b;
-		
-		
-	}
 	
 	
 	public static void pushHeap_min(int[] arr,int last, int item){
